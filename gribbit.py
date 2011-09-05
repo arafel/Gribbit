c9892773c982dd8bdb8a2a58794d785e30480c39
Paul Walker <gribbit@blacksun.org.uk>
2011-09-05 17:04:02 +0100

    do a little stuff

--- gribbit.py
+++ gribbit.py
@@ -1,2 +1,12 @@
-#!/bin/env python
+#!/usr/bin/python
 
+import tweepy
+from gribbit_keys import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tl = api.friends_timeline()
+for tweet in tl:
+    print "%s: %s" % (tweet.user.screen_name, tweet.text)
