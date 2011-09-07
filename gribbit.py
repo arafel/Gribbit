d28f9e8acff54b4a5553a1cfcd75e2d8b6b9d33d
Paul Walker <gribbit@blacksun.org.uk>
2011-09-07 16:42:23 +0100

    Now listens for 'HUP' to reload timeline early

--- gribbit.py
+++ gribbit.py
@@ -3,6 +3,7 @@
 MINUTE=60
 
 import time
+import signal, Queue
 import tabnanny
 
 import tweepy
@@ -12,9 +13,16 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+wakeup_queue = Queue.Queue()
+
+def handler(signum, frame):
+    print "Handler called"
+    wakeup_queue.put(signum)
+
 old_date = ""
 last_id = -1
 
+old_handler = signal.signal(signal.SIGHUP, handler)
 tl = api.friends_timeline()
 
 while 1:
@@ -29,5 +37,15 @@ while 1:
         print "%s - %s: %s" % (tweet.created_at.strftime(time_format), \
                                    tweet.user.screen_name, tweet.text)
         last_id = tweet.id
-    time.sleep(1 * MINUTE)
+    try:
+        item = wakeup_queue.get(True, 5 * MINUTE)
+        # If we reach this line we got a signal
+        print "Got signal", item
+        wakeup_queue.task_done()
+    except Queue.Empty:
+        # This is the usual case - timeout
+        pass
     tl = api.friends_timeline(since_id=last_id, count=1000)
+
+# Restore old handler
+signal.signal(signal.SIGHUP, old_handler)
